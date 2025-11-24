71714c99ee8a11c7bc748223d1cb2f14285cc73a
kethavatharjun <kethavatharjun0119@gmail.com>
2025-11-24 22:55:25 +0530

    Add project files.

---
+++ Vote/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Vote
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age<18)
+            {
+                throw new Exception("Not eligible for vote");
+            }
+            else
+            {
+                Console.WriteLine("Eligible for vote");
+            }
+        }
+    }
+}
